470f15165487b119b8a687dd59f96a6304cc9d03
Luis Cruz <lcruz@grupoq.com>
2017-01-17 23:42:09 -0600

    upd

--- Proyecto2/app.js
+++ Proyecto2/app.js
@@ -1,24 +1,75 @@
-var valor1 = 0, varlor2 = 0, operador = '';
+var valor1 = '', valor2 = '', operador = '';
+var limpiar = false;
+
+var inputs = document.getElementsByTagName('button');
+
+for(var i = 0; i < inputs.length; i++) {
+    inputs[i].onclick = function(){
+        presionarBoton(this);
+    };
+}
+
 
 function presionarBoton(boton)
 {
+  if (limpiar)
+  {
+    limpiar = false;
+    document.getElementById('resultado').value = "";
+  }
 
   if (boton.className == 'operador')
   {
-    if (valor1 == 0)
+    if (valor1 == '')
     {
       valor1 = document.getElementById('resultado').value
+      document.getElementById('resultado').value = "";
+
     }
     else {
       valor2 = document.getElementById('resultado').value
+      document.getElementById('resultado').value = "";
     }
 
-    if (boton.value == '=')
+    if ( boton.id != '=' )
     {
-        document.getElementById('resultado').value = valor1 + valor2;
+      operador = boton.id;
+    }
+
+    //Calcular
+    if (  valor2 != '' && valor1 != '' )
+    {
+        console.log( valor1 + ' ' +  operador + ' ' + valor2  );
+        switch (operador)
+        {
+          case '+':
+              document.getElementById('resultado').value = parseFloat(valor1) + parseFloat(valor2);
+              break;
+          case '-':
+              document.getElementById('resultado').value = parseFloat(valor1) - parseFloat(valor2);
+              break;
+          case '/':
+              document.getElementById('resultado').value = parseFloat(valor1) / parseFloat(valor2);
+              break;
+          case '*':
+              document.getElementById('resultado').value = parseFloat(valor1) * parseFloat(valor2);
+              break;
+          default:
+        }
+        valor1 = document.getElementById('resultado').value;
+        limpiar = true;
     }
   }
   else {
-    document.getElementById('resultado').value += 7;
+    document.getElementById('resultado').value += boton.id;
   }
+
+  if (boton.id == 'C') {
+    valor1 = '';
+    valor2 = '';
+    document.getElementById('resultado').value = '';
+    operador = '';
+
+  }
+
 }
